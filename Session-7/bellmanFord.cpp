9178ce29de480fe4305ee9176aeb4e7c10da68f0
Gauri Singh <149578403+gaurisingh73@users.noreply.github.com>
2026-03-25 12:21:33 +0530

    Create bellmanFord.cpp

---
+++ Session-7/bellmanFord.cpp
@@ -0,0 +1,20 @@
+vector<int> bellmanFord(int V, vector<vector<int>>& edges, int src) {
+	vector<int> dist(V, 1e8);
+	dist[src] = 0;
+  
+	for (int i = 0; i < V; i++) {
+	    
+		for (vector<int> edge : edges) {
+			int u = edge[0];
+			int v = edge[1];
+			int wt = edge[2];
+			if (dist[u] != 1e8 && dist[u] + wt < dist[v]) {
+                if(i == V - 1)
+                    return {-1};
+                dist[v] = dist[u] + wt;
+            }
+		}
+	}
+
+    return dist;
+}
